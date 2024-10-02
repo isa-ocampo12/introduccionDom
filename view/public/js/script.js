384dbb3dd8ed9feb9065d3224b7923348c859549
isa-ocampo12 <isaocampo174@gmail.com>
2024-10-02 16:10:02 -0500

    selectores

---
+++ view/public/js/script.js
@@ -0,0 +1,15 @@
+console.log(document.getElementById("usuario").textContent = prompt('Un nuevo Usuario'))
+
+
+// SELECTORES
+// getElement
+// querySelector 
+
+console.log(document.getElementById("usuario"))
+
+console.log(document.getElementsByClassName("form"))
+
+console.log(document.getElementsByTagName("form")) 
+
+console.log(document.querySelector("form")) 
+console.log(document.querySelectorAll("form")) 
